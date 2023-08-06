77c7217783de7d00c52787bffcba77c3691fa85a
Kono Steve <stevekono77@gmail.com>
2023-08-06 23:24:53 +0100

    Added mongoose and dotenv, server listens to requests only on connection to database

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// initialize app
 const app = express();
+const port = process.env.PORT || 4000;
 
+// Middleware
+app.use(express.json());
+
+
+// Routes
 app.get('/test',(req,res) => {
     res.json({ res:"Everything good!" });
 })
 
-app.listen(4000,() => {
-    console.log('Listening on port 4000');
-})
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(port,() => {
+        console.log(`Connected to db & listening on port ${port}`);
+    })
+});
