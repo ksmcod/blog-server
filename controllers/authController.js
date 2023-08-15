8f1782ece21a10f1d5459455031e9b093f9073e3
Kono Steve <stevekono77@gmail.com>
2023-08-15 21:58:07 +0100

    Created user model with mongoose

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,9 @@
 module.exports.register_post = async (req,res) => {
     const { username, email, password } = req.body;
-    console.log(username, email, password);
     res.status(200).json({ result:'success'})
 }
 
 module.exports.login_post = async(req,res) => {
     const { email, password } = req.body;
-    console.log(email,password);
     res.status(200).end("Success!");
 }
