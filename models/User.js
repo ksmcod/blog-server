67cdc5c6d27a1f36650cb48f5ab58d793f181cca
Kono Steve <stevekono77@gmail.com>
2023-08-15 22:24:23 +0100

    Created static function on user model to create a user

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -18,5 +20,35 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.statics.register = async function(username, email, password) {
+    if(!username || !email || !password) {
+        throw Error("All the fields must be filled!");
+    }
+
+    if(!validator.isEmail(email)) {
+        throw Error("Please enter a valid email!");
+    }
+
+    const usedUsername = this.find({ username });
+    const usedEmail = this.find({ email });
+
+    if(usedUsername) {
+        throw Error('This username is already in use!');
+    }
+
+    if(usedEmail) {
+        throw Error("This email is already in use!");
+    }
+
+    const salt = await bcrypt.genSalt(13)
+    const hash = await bcrypt.hash(password,salt);
+
+    const user = await this.create({ username, email, password:hash });
+    console.log(user);
+
+    return user;
+
+}
 
 const userModel = mongoose.model('user',userSchema);
