2f1c10b6b835ab60e4045873d5025695f283b51b
Kono Steve <stevekono77@gmail.com>
2023-08-16 04:33:28 +0100

    Implemented jsonwebtoken to return token on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,9 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+function createToken(payload) {
+    return jwt.sign({ payload },process.env.SECRET,{ expiresIn: 60 * 5 });
+}
 
 module.exports.register_post = async (req,res) => {
     const { username, email, password } = req.body;
@@ -14,8 +19,9 @@ module.exports.login_post = async(req,res) => {
     const { email, password } = req.body;
     try {
         const user = await User.login(email, password);
-        console.log('cookie: ',req.cookies);
-        res.cookie('jwt','token2',{ httpOnly:true, maxAge:1000 * 10 });
+        // console.log('cookie: ',req.cookies);
+        const token = createToken({ id:user._id });
+        res.cookie('jwt',token,{ httpOnly:true, maxAge:1000 * 60 * 10 });
         res.status(200).json({ user });
     } catch (error) {
         res.status(400).json({ error:error.message });
