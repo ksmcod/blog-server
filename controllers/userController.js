b7cb3febad05fb5808426905d0990cbc44ed91e6
Kono Steve <stevekono77@gmail.com>
2023-11-28 23:14:19 +0100

    created route to update user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,21 @@ module.exports.user_get = async (req, res) => {
   }
 };
 
+module.exports.user_post = async (req, res) => {
+  const { username, email, password } = req.body;
+  console.log(`username: ${username}, email: ${email}, password: ${password}`);
+
+  const { user_token } = req.cookies;
+
+  try {
+    const { id } = jwt.verify(user_token, PROCESS.ENV.SECRET);
+    const user = User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 module.exports.logout_post = async (req, res) => {
   const { user_token } = req.cookies;
   res.clearCookie("user_token");
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { user_get, logout_post } = require("../controllers/userController");
+const {
+  user_get,
+  logout_post,
+  user_post,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/user", user_get);
+router.post("/user", user_post);
 router.post("/logout", logout_post);
 
 module.exports = router;
